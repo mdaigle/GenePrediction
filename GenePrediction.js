d9ffe086e7368aa7135f5f798fcf961b66660e98
Malcolm Daigle <daiglemalcolm@gmail.com>
2016-11-03 09:57:33 -0700

    Fixes short and long ordering for faster first five. Better markov scores.

--- GenePrediction.js
+++ GenePrediction.js
@@ -3,7 +3,7 @@ var math = require('mathjs');
 
 math.config({
     number: 'BigNumber',
-    precision: 200
+    precision: 100
 });
 
 const assert = require('assert');
@@ -59,8 +59,9 @@ for (var start_index = 0; start_index < 3; start_index++) {
         for (var i = 0; i < 3; i++) {
             if (NUCLEOTIDES.indexOf(triplet[i].toUpperCase()) == -1)
             {
-                //console.log("changed nucleotide " + triplet[i]);
-                triplet = triplet.replace(triplet.charAt(i), "T");
+                var bad_char = triplet.charAt(i);
+                triplet = triplet.replace(bad_char, "T");
+                fasta_contents = fasta_contents.replace(bad_char, "T");
             }
         }
 
@@ -75,22 +76,21 @@ for (var start_index = 0; start_index < 3; start_index++) {
         // Short ORF threshold
         if (frame.length < 50) {
             num_short++;
-            short_sequences.push({
+            short_sequences[gene_start] = {
                 "frame": frame,
                 "start": gene_start + 1,
                 "length": frame.length
-            });
+            };
         }
 
         // Long ORF threshold
         if (frame.length > 1400) {
             num_long++;
-            long_sequences.push({
+            long_sequences[gene_start] = {
                 "frame": frame,
                 "start": gene_start + 1,
                 "length": frame.length
-            });
-            //console.log("found long at " + (gene_start + 1));
+            };
         }
 
         // Update gene start index
@@ -140,7 +140,7 @@ short_sequences.forEach(countSequences, q_counts);
 calculateProbs(p_counts, p_probs);
 calculateProbs(q_counts, q_probs);
 
-console.log("Prob of A given AAA = " + p_probs[0][0][0][0]);
+console.log("Prob of A given AAA = " + p_probs[0][0][0][1]);
 
 for (var j = 0; j < 4; j++) {
     for (var k = 0; k < 4; k++) {
@@ -158,10 +158,19 @@ for (var j = 0; j < 4; j++) {
 
 long_sequences.forEach(calculateGeneProbability, p_probs);
 
+//console.log("done with long seq probs");
+
+short_sequences.forEach(calculateGeneProbability, q_probs);
+
+//console.log("done calculating gene probs");
+
 printFirstFive(long_sequences);
 printFirstFive(short_sequences);
 
 function countSequences(sequence_record, index, arr, prob_array) {
+    if (sequence_record == undefined) {
+        return;
+    }
     var sequence = sequence_record.frame;
     for (var i = 0; i + 3 < sequence.length - 3; i++) {
         for (j = 0; j < 3; j++) {
@@ -212,6 +221,9 @@ function calculateProbs(count_arr, prob_arr) {
 }
 
 function calculateGeneProbability(sequence_record, index, arr) {
+    if (sequence_record == undefined) {
+        return;
+    }
     var sequence = sequence_record.frame;
     //console.log(sequence.length);
     //console.log("probs");
@@ -220,7 +232,7 @@ function calculateGeneProbability(sequence_record, index, arr) {
     var p_prob = math.bignumber(1);
     var q_prob = math.bignumber(1);
 
-    if (sequence.length > 3) {
+    if (sequence_record['length'] > 3) {
         var first = NUCLEOTIDES.indexOf(sequence.charAt(0));
         var second = NUCLEOTIDES.indexOf(sequence.charAt(1));
         var third = NUCLEOTIDES.indexOf(sequence.charAt(2));
@@ -235,13 +247,14 @@ function calculateGeneProbability(sequence_record, index, arr) {
     }
 
 
-    for (var i = 3; i < sequence.length - 3; i++) {
+    for (var i = 3; i < sequence_record['length'] - 3; i++) {
         var first = NUCLEOTIDES.indexOf(sequence.charAt(i - 3));
         var second = NUCLEOTIDES.indexOf(sequence.charAt(i - 2));
         var third = NUCLEOTIDES.indexOf(sequence.charAt(i - 1));
-        var curr = NUCLEOTIDES.indexOf(sequence.charAt(i));
+        // Add one to skip past the 3-mer prob
+        var curr = NUCLEOTIDES.indexOf(sequence.charAt(i)) + 1;
 
-        assert(curr >= 0 && curr < 4, "char is " + sequence.charAt(i));
+        assert(curr > 0 && curr <= 4, "char is " + sequence.charAt(i));
         assert(p_probs[first][second][third][curr]);
         assert(q_probs[first][second][third][curr]);
 
@@ -252,39 +265,21 @@ function calculateGeneProbability(sequence_record, index, arr) {
     var div = math.divide(math.bignumber(p_prob), math.bignumber(q_prob));
     var markov_score = math.log(div, math.bignumber(2));
 
-    /*if (index < 5) {
-        console.log(markov_score.toString());
-    }*/
     arr[index].score = markov_score;
 }
 
 function printFirstFive(seq_arr) {
-    var min_index = [];
-    for (var i = 0; i < 5; i++) {
-        if (i == 0){
-            min_index[i] = 0;
+    var first_five_sequences = [];
+    var index = 0;
+    for (var i = 0; i < seq_arr.length && index < 5; i++) {
+        if (seq_arr[i] == undefined) {
+            continue;
         }
-        for (var j = 0; j < seq_arr.length; j++) {
-            if (i == 0) {
-                if (seq_arr[j].start < seq_arr[min_index[0]].start) {
-                    min_index[0] = j;
-                }
-            } else {
-                if (min_index[i] == undefined && seq_arr[j].start > seq_arr[min_index[i-1]].start) {
-                    min_index[i] = j
-                } else {
-                    assert(seq_arr[min_index[i]], "undefined at index" + min_index[i]);
-                    if (seq_arr[j].start < seq_arr[min_index[i]].start &&
-                        seq_arr[j].start > seq_arr[min_index[i-1]].start) {
-                            min_index[i] = j;
-                    }
-                }
-            }
-        }
+        first_five_sequences[index++] = seq_arr[i];
     }
 
     for (var i = 0; i < 5; i++) {
-        var sequence = seq_arr[min_index[i]];
+        var sequence = first_five_sequences[i];
         console.log(i + " " + sequence['start'] + " " + sequence['length'] + " " + sequence['score']);
     }
 }
